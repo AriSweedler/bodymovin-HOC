31524287df8fe9e959d4773ce17f372fc99e7b6d
Ari Sweedler <ari@sweedler.com>
2017-05-12 14:35:30 -0700

    Displays two static json files as animated SVGs
    I found/downloaded two bodymovin files. As they’re saved as JSON files,
    I couldn’t display them without parsing them into SVG elements.
    Bodymovin actually gives you a special javascript file to do this for
    you, so I just linked that script in the head of my index file.
    
    All the work I did went into the “displayTwoAnimations” js file, but
    it’s going to become obsolete soon when I write it in React. I needed
    to learn what the heck was happening before I tried to do stuff in
    React. Now that I’ve got this base I can start writing a React HOC to
    replace what I have. It’ll be a lot nicer

---
+++ displayTwoAnimations.js
@@ -0,0 +1,30 @@
+var animNumber = 0;
+
+function createNewAnimBlock() {
+    let homeBlock = document.getElementById("home");
+    let myAnimationBlock = document.createElement("div");
+    myAnimationBlock.setAttribute("class", "bodymovinHost"); 
+    myAnimationBlock.setAttribute("id", "bodymovin_" + animNumber); 
+    homeBlock.appendChild(myAnimationBlock);
+}
+
+function doAnimation(myPath){
+    createNewAnimBlock();
+    var anim;
+    var animData = {
+        container: document.getElementById("bodymovin_" + animNumber),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        autoloadSegments: true,
+        rendererSettings: {
+            progressiveLoad:false
+        },
+        path: myPath
+    };
+    anim = bodymovin.loadAnimation(animData);
+    animNumber++;
+}
+
+doAnimation('animations/firstAnimation.json');       
+doAnimation('animations/secondAnimation.json');
